7b0ec4c2727972e9b6227db4f593232bc685a011
James Brett <cronus@MBP.lan>
2018-11-12 16:17:13 +0000

    Seperate cards into lists and display them better

--- artifact-forge/src/app/app.component.ts
+++ artifact-forge/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DeckService } from './services/deck.service';
 import { Cdn } from './model/cdn';
 import { CardCollection } from './model/card-collection';
+import { Card } from './model/card';
 
 @Component({
     selector: 'app-root',
@@ -11,6 +12,11 @@ export class AppComponent implements OnInit {
 
     cdn: Cdn;
     cardCollection: CardCollection;
+    heroList: Card[];
+    passiveAbilityList: Card[];
+    spellList: Card[];
+    creepList: Card[];
+    abilityList: Card[];
 
     constructor(private deckService: DeckService) { }
 
@@ -19,6 +25,11 @@ export class AppComponent implements OnInit {
             this.cdn = cdn;
             this.deckService.getAllCards(this.cdn).subscribe(cardCollection => {
                 this.cardCollection = cardCollection;
+                this.heroList = this.cardCollection.card_set.card_list.filter(c => c.card_type === 'Hero');
+                this.passiveAbilityList = this.cardCollection.card_set.card_list.filter(c => c.card_type === 'Passive Ability');
+                this.spellList = this.cardCollection.card_set.card_list.filter(c => c.card_type === 'Spell');
+                this.creepList = this.cardCollection.card_set.card_list.filter(c => c.card_type === 'Creep');
+                this.abilityList = this.cardCollection.card_set.card_list.filter(c => c.card_type === 'Ability');
             });
         });
     }
